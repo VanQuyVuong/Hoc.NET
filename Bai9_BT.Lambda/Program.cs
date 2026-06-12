6d8d427803a1429ad02567f1b4a7251e028cfc8d
VANQUYVUONG <vanbin1605@gmail.com>
2026-06-12 12:34:05 +0700

    Bài 9, biểu thức lambda

---
+++ Bai9_BT.Lambda/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Bai9_BT.Lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sum = (int a, int b) => a + b;  //cách viết lambda expression để tính tổng hai số nguyên a và b
+            Func<int, int, int>  count = (int c, int d) => c + d; //cách viết lambda expression sử dụng Func delegate để tính tổng hai số nguyên a và b
+            Console.WriteLine(sum(3,6));
+            Console.WriteLine(count(10, 9));
+        }
+    }
+}

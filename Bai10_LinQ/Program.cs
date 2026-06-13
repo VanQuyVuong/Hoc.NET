eb7f66986481a39d253b2cd7622f70e8663dfae6
VANQUYVUONG <vanbin1605@gmail.com>
2026-06-13 18:03:02 +0700

    LinQ đầu tiên ,, câi select n >=0 từ dãy dữ liệu có sẵn

--- Bai10_LinQ/Program.cs
+++ Bai10_LinQ/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace Bai10_LinQ
+﻿using System.Data.Common;
+
+namespace Bai10_LinQ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var dataSource = GetIntNumbers();
+            //Console.WriteLine("Hello, World!");
+            Print(dataSource);
+
+            var ns = from n in dataSource
+                     where n >= 0
+                     select n;
+            Print(ns);
+        }
+
+
+        static IEnumerable<int> GetIntNumbers()
+        {
+            var ns = new int[] { 1, 2, 3, 4, 898, 56, 8987, 3459, -1, -987, 55, -7 };
+            return ns;
+        }
+
+        static void Print(IEnumerable<int> values)
+        {
+
+            Console.WriteLine("----------------");
+
+            foreach (var value in values)
+            {
+                Console.WriteLine(value);
+            }
         }
     }
 }
+

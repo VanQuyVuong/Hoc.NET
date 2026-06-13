5eff451fe895b10f76ef71a308069bd49a0dde27
VANQUYVUONG <vanbin1605@gmail.com>
2026-06-13 17:51:03 +0700

    Tạo project LinQ

--- Bai9_BT.Lambda/Program.cs
+++ Bai9_BT.Lambda/Program.cs
@@ -8,6 +8,16 @@
             Func<int, int, int>  count = (int c, int d) => c + d; //cách viết lambda expression sử dụng Func delegate để tính tổng hai số nguyên a và b
             Console.WriteLine(sum(3,6));
             Console.WriteLine(count(10, 9));
+
+
+            var printUpper = (String s) => Console.WriteLine(s.ToUpper());
+
+            printUpper("dvasdfhhg jhadugsa ádgesad jhavsdjsa ");
+
+
+            var t = object (int a, int b) => a > b ? 0 : "A";
+            Console.WriteLine(t(1, 2));
+            Console.WriteLine(t(2, 1));
         }
     }
 }

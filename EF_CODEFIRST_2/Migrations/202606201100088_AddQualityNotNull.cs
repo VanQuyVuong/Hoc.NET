bdc877827ab8891e35b02ad9ce0ac27799752ede
VANQUYVUONG <vanbin1605@gmail.com>
2026-06-20 18:03:45 +0700

    Thêm dữ liệu Quality với giá trị notnull và thêm dèaultvalue =1

---
+++ EF_CODEFIRST_2/Migrations/202606201100088_AddQualityNotNull.cs
@@ -0,0 +1,18 @@
+﻿namespace EF_CODEFIRST_2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddQualityNotNull : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Products", "Quality", c => c.Long(nullable: false, defaultValue: 1));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Products", "Quality");
+        }
+    }
+}
